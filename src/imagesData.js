091a8c2d272cbcc883346f6fd0c1f475dad7ac8c
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-08-12 18:05:24 +0530

    Update imagesData.js
    albumId id changed

--- src/imagesData.js
+++ src/imagesData.js
@@ -1,118 +1,118 @@
 const ImagesData= [
     {
-      "albumId": 1,
+      "albumId": "Gallery 1",
       "id": 1,
       "title": "accusamus beatae ad facilis cum similique qui sunt",
       "url": "https://via.placeholder.com/600/92c952",
       "thumbnailUrl": "https://via.placeholder.com/150/92c952"
     },
     {
-      "albumId": 1,
+      "albumId": "Gallery 1",
       "id": 2,
       "title": "reprehenderit est deserunt velit ipsam",
       "url": "https://via.placeholder.com/600/771796",
       "thumbnailUrl": "https://via.placeholder.com/150/771796"
     },
     {
-      "albumId": 1,
+      "albumId": "Gallery 1",
       "id": 3,
       "title": "officia porro iure quia iusto qui ipsa ut modi",
       "url": "https://via.placeholder.com/600/24f355",
       "thumbnailUrl": "https://via.placeholder.com/150/24f355"
     },
     {
-      "albumId": 1,
+      "albumId": "Gallery 1",
       "id": 4,
       "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
       "url": "https://via.placeholder.com/600/d32776",
       "thumbnailUrl": "https://via.placeholder.com/150/d32776"
     },
     {
-      "albumId": 1,
+      "albumId": "Gallery 1",
       "id": 5,
       "title": "natus nisi omnis corporis facere molestiae rerum in",
       "url": "https://via.placeholder.com/600/f66b97",
       "thumbnailUrl": "https://via.placeholder.com/150/f66b97"
     },
     {
-      "albumId": 1,
+      "albumId": "Gallery 1",
       "id": 6,
       "title": "accusamus ea aliquid et amet sequi nemo",
       "url": "https://via.placeholder.com/600/56a8c2",
       "thumbnailUrl": "https://via.placeholder.com/150/56a8c2"
     },
     {
-        "albumId": 2,
+        "albumId": "Gallery 2",
         "id": 51,
         "title": "non sunt voluptatem placeat consequuntur rem incidunt",
         "url": "https://via.placeholder.com/600/8e973b",
         "thumbnailUrl": "https://via.placeholder.com/150/8e973b"
       },
       {
-        "albumId": 2,
+        "albumId": "Gallery 2",
         "id": 52,
         "title": "eveniet pariatur quia nobis reiciendis laboriosam ea",
         "url": "https://via.placeholder.com/600/121fa4",
         "thumbnailUrl": "https://via.placeholder.com/150/121fa4"
       },
       {
-        "albumId": 2,
+        "albumId": "Gallery 2",
         "id": 53,
         "title": "soluta et harum aliquid officiis ab omnis consequatur",
         "url": "https://via.placeholder.com/600/6efc5f",
         "thumbnailUrl": "https://via.placeholder.com/150/6efc5f"
       },
       {
-        "albumId": 2,
+        "albumId": "Gallery 2",
         "id": 54,
         "title": "ut ex quibusdam dolore mollitia",
         "url": "https://via.placeholder.com/600/aa8f2e",
         "thumbnailUrl": "https://via.placeholder.com/150/aa8f2e"
       },
       {
-        "albumId": 2,
+        "albumId": "Gallery 2",
         "id": 55,
         "title": "voluptatem consequatur totam qui aut iure est vel",
         "url": "https://via.placeholder.com/600/5e04a4",
         "thumbnailUrl": "https://via.placeholder.com/150/5e04a4"
       },
       {
-        "albumId": 2,
+        "albumId": "Gallery 2",
         "id": 56,
         "title": "vel voluptatem esse consequuntur est officia quo aut quisquam",
         "url": "https://via.placeholder.com/600/f9f067",
         "thumbnailUrl": "https://via.placeholder.com/150/f9f067"
       },
       {
-        "albumId": 3,
+        "albumId": "Gallery 3",
         "id": 101,
         "title": "incidunt alias vel enim",
         "url": "https://via.placeholder.com/600/e743b",
         "thumbnailUrl": "https://via.placeholder.com/150/e743b"
       },
       {
-        "albumId": 3,
+        "albumId": "Gallery 3",
         "id": 102,
         "title": "eaque iste corporis tempora vero distinctio consequuntur nisi nesciunt",
         "url": "https://via.placeholder.com/600/a393af",
         "thumbnailUrl": "https://via.placeholder.com/150/a393af"
       },
       {
-        "albumId": 3,
+        "albumId": "Gallery 3",
         "id": 103,
         "title": "et eius nisi in ut reprehenderit labore eum",
         "url": "https://via.placeholder.com/600/35cedf",
         "thumbnailUrl": "https://via.placeholder.com/150/35cedf"
       },
       {
-        "albumId": 4,
+        "albumId": "Gallery 4",
         "id": 151,
         "title": "possimus dolor minima provident ipsam",
         "url": "https://via.placeholder.com/600/1d2ad4",
         "thumbnailUrl": "https://via.placeholder.com/150/1d2ad4"
       },
       {
-        "albumId": 4,
+        "albumId": "Gallery 4",
         "id": 152,
         "title": "et accusantium enim pariatur eum nihil fugit",
         "url": "https://via.placeholder.com/600/a01c5b",
