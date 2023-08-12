a169a0560ecfc48cec29c1597138794c941422fe
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-08-12 18:07:17 +0530

    Update page.js
    Button component added to Page component

--- src/page.js
+++ src/page.js
@@ -1,27 +1,36 @@
 import React,{useState} from 'react'
 import ImagesData from './imagesData'
 import Tiles from './components/Tiles'
+import Buttons from './components/Buttons'
 // import CardsTiles from './components/CardsTiles'
 
 const Page = () => {
   const [items, setItems]=useState(ImagesData)
   const handleClick = (galleryId)=>{
     const filteredItems= ImagesData.filter(item=>{
-      return item.albumId===galleryId
+      if(galleryId!=="All"){
+        return item.albumId===galleryId
+      }
+      else{
+        return ImagesData
+      }
     })
     setItems(filteredItems)
   }
+
+  const uniqueItems = [...new Set(ImagesData.map(item=>item.albumId)),'All']
   return (
     <>
       <h1 className='text-3xl font-bold mt-8 mb-2'>Images Gallery</h1>
       <hr/>
       <nav>
         <ul className='grid grid-cols-5 md:grid-cols-5 sm:grid-cols-3'>
-          <li className='mx-4'><button className='btn btn-blue' onClick={()=>handleClick(1)}>Gallery 1</button></li>
+          {uniqueItems.map(data=> <li key={data} className='mx-4'> <Buttons onClick={()=>handleClick(data)} text={data}/></li>)}
+          {/* <li className='mx-4'><button className='btn btn-blue' onClick={()=>handleClick(1)}>Gallery 1</button></li>
           <li className='mx-4'><button className='btn btn-blue' onClick={()=>handleClick(2)}>Gallery 2</button></li>
           <li className='mx-4'><button className='btn btn-blue' onClick={()=>handleClick(3)}>Gallery 3</button></li>
           <li className='mx-4'><button className='btn btn-blue' onClick={()=>handleClick(4)}>Gallery 4</button></li>
-          <li className='mx-4'><button className='btn btn-blue' onClick={()=>setItems(ImagesData)}>All</button></li>
+          <li className='mx-4'><button className='btn btn-blue' onClick={()=>setItems(ImagesData)}>All</button></li> */}
         </ul>
       </nav>
       <div className='grid grid-cols-3 md:grid-cols-3 sm:grid-cols-1 gap-4 mx-4'>
